afd2bb46fcadd076dee49adf422a6ddbe55d5d21
Mohammed Saabith Cp <44156514+mohdsaabith@users.noreply.github.com>
2022-09-11 16:35:43 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let images = ["/assets/images/dice-01.svg",
+"/assets/images/dice-02.svg",
+"/assets/images/dice-03.svg",
+"/assets/images/dice-04.svg",
+"/assets/images/dice-05.svg",
+"/assets/images/dice-06.svg"];
+
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake")
+        });
+
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+
+        console.log(dieOneValue,dieTwoValue);
+
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src",images[dieTwoValue]);
+   
+        document.querySelector("#total-1").innerHTML = "Player-1 have " + (dieOneValue + 1);
+        document.querySelector("#total-2").innerHTML = "Player-2 have " + (dieTwoValue + 1 );
+    },
+    1000
+    );
+}
