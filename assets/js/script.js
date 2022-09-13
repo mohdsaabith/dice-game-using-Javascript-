b90c8af9ffea6122016be172b99615ceb2978773
Mohammed Saabith Cp <44156514+mohdsaabith@users.noreply.github.com>
2022-09-13 08:52:35 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,17 +27,22 @@ function roll(){
         });
 
         //here is the math random funtion it call's random number upto 6
+
+        //Here we add the score of 1st dice
         let dieOneValue = Math.floor(Math.random()*6);
         diceScore1 = dieOneValue + 1;
         currentScore1 += diceScore1;
+        //if the player-1 have scored 25 first. than, here is the function to screen the player-1 won
         if(currentScore1 >=25){
             document.querySelector("#result").innerHTML = "Player-1 won";
         }
+        
 
-
+        //Here we add the the score of 2nd dice
         let dieTwoValue = Math.floor(Math.random()*6);
         diceScore2 = dieTwoValue + 1;
         currentScore2 += diceScore2;
+        //if the player-2 have scored 25 first. than, here is the function to screen the player-2 won
         if(currentScore2 >= 25){
             document.querySelector("#result").innerHTML = "Player-2 won";
         }
@@ -57,6 +62,7 @@ function roll(){
     );
 }
 
+//here is the reset button
 function reset(){
     var total1 =0;
     var total2 = 0;
@@ -64,8 +70,10 @@ function reset(){
     currentScore1 = 0;
     currentScore2 = 0;
 
+    // here we make the score again 0
     document.querySelector("#total-1").innerHTML = "Player-1 have " + 0 ;
     document.querySelector("#total-2").innerHTML = "Player-2 have " + 0  ;
 
+    //here we disappear who won
     document.querySelector("#result").innerHTML = null;
 }
